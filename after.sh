1f63a27f0cee22307118953d92c5b91e1828dfbf
Mohammed-Alama <mohammedalsayed9688@gmail.com>
2019-12-23 14:31:52 +0200

    Initialize Composer dependencies and additional files.

---
+++ after.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+echo " Installing Composer Dependencies "
+composer require --dev barryvdh/laravel-ide-helper
+echo "ide-helper installed success"
+echo "begin ide-helper config"
+php artisan ide-helper:generate
+php artisan vendor:publish --provider="Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider" --tag=config
+php artisan ide-helper:models
+php artisan ide-helper:meta
+echo "ide-helper config success"
+echo " Committing after modifications to Git "
+git add .
+git commit -am "Initialize Composer dependencies and additional files."
